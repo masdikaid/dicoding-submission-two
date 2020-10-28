b43663a87e984d08979a4d08a3e4abb6757beefe
masdika ID <masdikaid@gmail.com>
2020-10-28 19:20:30 +0700

    reconstructuring code

---
+++ js/fetchapi.js
@@ -0,0 +1,33 @@
+
+function fetchData(path, options=null){
+    let optionsdata = { "headers":{ "X-Auth-Token":"4e9d2e97f7a64fce84dce406786a5d8b"}}
+    if(options){
+      optionsdata = { ...optionsdata, ...options}
+    }
+    return new Promise((resolve, reject) =>{
+      fetch(`http://api.football-data.org${path}`, optionsdata)
+      .then(response => {
+        resolve(response.json())
+      })
+      .catch(error => {
+        reject(error)
+      });
+    });
+};
+
+function getPage(page){
+    return new Promise((resolve, reject)=>{
+      const xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = () => {
+        if (xhttp.readyState === 4 && xhttp.status === 200) {
+          resolve(xhttp.responseText);
+        }
+        if (xhttp.status === 404) {
+          reject("Something Went Wrong !")
+        }
+      };
+      xhttp.open("GET", `pages/${page}`, true);
+      xhttp.send();
+    });
+  };
+  
