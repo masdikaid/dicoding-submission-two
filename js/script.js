1556bfa479be2f9d9f4e6bb45d35837d525e8c60
masdika ID <masdikaid@gmail.com>
2020-10-29 13:39:30 +0700

    update push notification features

--- js/script.js
+++ js/script.js
@@ -22,3 +22,22 @@ document.addEventListener('DOMContentLoaded', function() {
   getCompetitionPage();
   navHandler();
 });
+
+if (('PushManager' in window)) {
+  navigator.serviceWorker.getRegistration().then(function(registration) {
+      registration.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: urlBase64ToUint8Array("BIIfdWdLrx6gS6GSCpBOqmSGyCGHtk2JPbwtVk462QDaZo66tw56EMm_5ShoudSbtOs1WKYsP95IHIUnrybwXuk")
+      })
+      .then(function(subscribe) {
+          console.log('Berhasil melakukan subscribe dengan endpoint: ', subscribe.endpoint);
+          console.log('Berhasil melakukan subscribe dengan p256dh key: ', btoa(String.fromCharCode.apply(
+              null, new Uint8Array(subscribe.getKey('p256dh')))));
+          console.log('Berhasil melakukan subscribe dengan auth key: ', btoa(String.fromCharCode.apply(
+              null, new Uint8Array(subscribe.getKey('auth')))));
+      })
+      .catch(function(e) {
+          console.error('Tidak dapat melakukan subscribe ', e.message);
+      });
+  });
+};
--- sw.js
+++ sw.js
@@ -37,7 +37,6 @@ self.addEventListener("fetch", function(event) {
             if (response) {
                 return response;
             }
-
             const fetchrequest = event.request.clone();
             return fetch(fetchrequest).then( response => {
                 if(!response || response.status !== 200) {
@@ -53,3 +52,24 @@ self.addEventListener("fetch", function(event) {
         })
     );
 });
+
+self.addEventListener('push', function(event) {
+    let body;
+    if (event.data) {
+      body = event.data.text();
+    } else {
+      body = 'Push message no payload';
+    }
+    const options = {
+      body: body,
+      icon: 'assets/icon.png',
+      vibrate: [100, 50, 100],
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: 1
+      }
+    };
+    event.waitUntil(
+      self.registration.showNotification('Soccer Maniacs | Pertandingan', options)
+    );
+  });
